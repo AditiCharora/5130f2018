8d3fd9b85448761ff3ea43b48b71f6366b4c8a5b
Aditi Charora <aditicharora@Aditis-MacBook-Pro.local>
2018-11-24 18:10:19 -0500

    Removed duplicate folder structure

--- LinkedInDemo/src/linkedin/credentials.java
+++
@@ -1,9 +0,0 @@
-package linkedin;
-
-public class credentials {
-
-	public static String client_id="781v2ovwhpmy7y";
-	public static String redirect_url="http://localhost:8080/linkedin/success.jsp";
-	public static String client_secret="Q0LUU8ENVfugQw3w";
-	
-}
--- Onboard/src/linkedin/credentials.java
+++
@@ -1,9 +0,0 @@
-package linkedin;
-
-public class credentials {
-
-	public static String client_id="********************";
-	public static String redirect_url="http://localhost:8080/linkedin/success.jsp";
-	public static String client_secret="****************************************";
-
-}
