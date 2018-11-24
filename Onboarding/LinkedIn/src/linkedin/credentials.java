1bba4111710b48f4b16938c1dfcb974abbfab73b
Aditi Charora <42656421+AditiCharora@users.noreply.github.com>
2018-11-24 18:16:20 -0500

    Established connection between client and linkedin

---
+++ Onboarding/LinkedIn/src/linkedin/credentials.java
@@ -0,0 +1,9 @@
+package linkedin;
+
+public class credentials {
+
+	public static String client_id="********************";
+	public static String redirect_url="http://localhost:8080/linkedin/success.jsp";
+	public static String client_secret="****************************************";
+
+}
