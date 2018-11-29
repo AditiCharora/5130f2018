3917c19872ad3f47688d30b2282345ef38228e30
Aditi Charora <42656421+AditiCharora@users.noreply.github.com>
2018-11-28 20:10:12 -0500

    added integration for facebook

---
+++ Onboarding/facebook/FacebookController.java
@@ -0,0 +1,19 @@
+package com.onboarding.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/facebook")
+
+public class FacebookController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView facebook()
+	{
+		return new ModelAndView("index");
+	}
+
+}
